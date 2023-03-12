8d1e555e85d3410c407d4afcca9a0186598ea01d
dan.agustinus@gmail.com <dan.agustinus@gmail.com>
2023-03-11 23:57:31 -0800

    Updated log to debug and cleaned up hints

--- app/datablock_genie.py
+++ app/datablock_genie.py
@@ -14,27 +14,29 @@ class DataBlockGenie:
         self.logger = get_logger(log_level)
         self._data_gen = FakerGen()
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         columns = self.data_template.columns.keys()
-        
+
         if columns:
             all_columns = "".join([f"\n|-- {col}" for col in columns])
         else:
             all_columns = "-- No Columns added yet."
         return f"Dataframe row counts: {self.row_counts}.\nColumns: {all_columns}"
 
-    def set_row_counts(self, row_count: int):
+    def set_row_counts(self, row_count: int) -> None:
         self.row_counts = row_count if row_count != self.row_counts else self.row_counts
 
     def create_spark_df(self, spark_session: SparkSession) -> DataFrame:
-        rows, columns = self.generate_data()
+        rows, columns = self._generate_data()
         df = spark_session.createDataFrame(rows, columns)
+        self.logger.debug("Spark DF has been created")
 
         return df
     
     def create_pandas_df(self) -> pd.DataFrame:
-        rows, columns = self.generate_data()
+        rows, columns = self._generate_data()
         df = pd.DataFrame(data=rows, columns=columns)
+        self.logger.debug("Pandas DF has been created")
 
         return df
     
@@ -51,17 +53,17 @@ class DataBlockGenie:
             for col in cols:
                 if col in temp_cols.keys():
                     del temp_cols[col]
-                    self.logger.info(f'Column "{col}" has been deleted')
+                    self.logger.debug(f'Column "{col}" has been deleted')
                 else:
                     self.logger.warning(f'Column "{col}" was not found')
         else:
             if cols in temp_cols.keys():
                 del temp_cols[cols]
-                self.logger.info(f'Column "{cols}" has been deleted')
+                self.logger.debug(f'Column "{cols}" has been deleted')
             else:
                 self.logger.warning(f'Column "{cols}" was not found')
 
-    def generate_data(self) -> tuple:
+    def _generate_data(self) -> tuple:
         amount = self.data_template.amount
 
         column_names = [column_name for column_name in self.data_template.columns.keys()]
@@ -78,7 +80,3 @@ class DataBlockGenie:
             rows.append(row_column)
 
         return rows, column_names
-
-
-
-
